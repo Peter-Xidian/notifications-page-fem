976599a25abaf7e76e8d382e7870ada339e8d6e0
Peter-Xidian <mayowaodebode@gmail.com>
2022-12-05 13:00:08 +0200

    JS Commit 3

--- README.md
+++ README.md
@@ -33,8 +33,8 @@ Users should be able to:
 
 ### Links
 
-- Solution URL: [Solution URL](https://qr-code-component.github.io/)
-- Live Site URL: [Live Site URL](https://qr-code-component.github.io/)
+- Solution URL: [Solution URL](https://notification-component.netlify.app/)
+- Live Site URL: [Live Site URL](https://notification-component.netlify.app/)
 
 ## My process
 
--- index.js
+++ index.js
@@ -6,11 +6,18 @@ function toggleUnread() {
   let notifNum = document.querySelector(".notifs-number");
   let nIcons = document.querySelectorAll(".notif-icon");
   let nmCards = document.querySelectorAll(".nm-card");
+  let nMark = document.querySelector(".mark-toggle");
 
   if (notifNum.innerHTML === "3") {
     notifNum.innerHTML = "0";
   } else if (notifNum.innerHTML === "0") {
     notifNum.innerHTML = "3";
+  }
+
+  if (nMark.innerHTML === "Mark all as read") {
+    nMark.innerHTML = "Mark as unread";
+  } else if (nMark.innerHTML === "Mark as unread") {
+    nMark.innerHTML = "Mark all as read";
   }
 
   for (let i = 0; i < 3; i++) {
